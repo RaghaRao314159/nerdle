5218f0edb6cd8e17eaabff40b7d66f394d63d24f
liuzihe02 <liuzihenanhua@gmail.com>
2025-11-01 18:18:39 +0000

    nerdle stuff

--- README.md
+++ README.md
@@ -22,28 +22,63 @@ Nerdle solvers
 
 Scrape through all these codebases using your github tools to determine whats the optimum library to use (most comprehensive and well written code)
 
-<fetch_link>
-https://github.com/pedrokkrause/Nerdle-Equations
-https://github.com/lfhohmann/nerdle-data-analysis
-https://github.com/starypatyk/nerdle-solver
-</fetch_link>
+> These instructions assume you are running from the parent folder so you can view multipe repos at once. I have cloned all these repos locally so you can refer to them
 
+## Nerdle PedrokRause
+
+https://github.com/pedrokkrause/Nerdle-Equations
+
+<read_code>
+Nerdle-Equations/MaxiGenerator.py
+Nerdle-Equations/NerdleGenerator.py
+Nerdle-Equations/README.md
+Nerdle-Equations/wordle.py
+</read_code>
+
+## Nerdle Data Analysis
+
+https://github.com/lfhohmann/nerdle-data-analysis
+
+<read_code>
+nerdle-data-analysis/0.equations_generator.ipynb
+nerdle-data-analysis/2.simulator_mini_nerdle_random.ipynb
+<read_code>
+
+## Nerdle Solver
+
+https://github.com/starypatyk/nerdle-solver
+
+<read_code>
+nerdle-solver/libnerdle.py
+nerdle-solver/gen_perms.py
+<read_code>
 
 ## Unsloth AI
 
-We'll be using unsloth AI to set up our training pipeline. Here's an example notebook
+We'll be using unsloth AI to set up our training pipeline. Here's some example notebooks:
 
-<fetch_link>
-https://colab.research.google.com/github/unslothai/notebooks/blob/main/nb/Qwen3_(4B)-GRPO.ipynb
+<read_link>
 https://docs.unsloth.ai/get-started/fine-tuning-llms-guide/tutorial-how-to-finetune-llama-3-and-use-in-ollama
+</read_link>
+
+for this notebook read
+
+https://colab.research.google.com/github/unslothai/notebooks/blob/main/nb/Qwen3_(4B)-GRPO.ipynb
+<read_code>
+nerdle/unsloth_sample_notebooks/Qwen3_(4B)_GRPO.ipynb
+</read_code>
+
+for this notebook read
 https://colab.research.google.com/github/unslothai/notebooks/blob/main/nb/Advanced_Llama3_2_(3B)_GRPO_LoRA.ipynb
-</fetch_link>
+<read_code>
+nerdle/unsloth_sample_notebooks/Advanced_Llama3_2_(3B)_GRPO_LoRA.ipynb
+</read_code>
 
 You can also refer to the existing notebook that we use currently
 
 <read_code>
-GRPO.ipynb
-dataset.jsonl
+nerdle/dataset.jsonl
+nerdle/GRPO.ipynb
 </read_code>
 
 ## Wordle Examples
@@ -54,5 +89,4 @@ Here are some examples of RL training already on previous games wordle
 https://github.com/RLDiary/Wordle-GRPO
 https://github.com/shreyansh26/wordle-solver
 </fetch_link>
-
 
---
+++ solver/wordle.py
@@ -0,0 +1,84 @@
+from math import log2
+from tqdm import tqdm
+from collections import defaultdict
+from time import sleep
+from copy import deepcopy
+
+def genmaskW(expression,correct):
+    mask = ['N']*len(expression)
+    count = defaultdict(lambda: 0)
+    for i, x in enumerate(expression):
+        if x == correct[i]:
+            mask[i] = '2'
+            count[x] += 1
+    for i,x in enumerate(expression):
+        if x not in correct:
+            mask[i] = '0'
+        elif x != correct[i]:
+            if count[x] < correct.count(x):
+                mask[i] = '1'
+                count[x] += 1
+            else:
+                mask[i] = '0'
+    return(''.join(mask))
+
+def checkmaskW(mask,expression,correct):
+    if mask == genmaskW(expression,correct):
+        return(True)
+    else:
+        return(False)
+
+def allmasksW(expression,dictionary):
+    masks = defaultdict(lambda: 0)
+    for expression2 in dictionary:
+        mask = genmaskW(expression,expression2)
+        masks[mask] += 1
+    return(masks)
+
+def filtermaskW(mask,expression,dictionary):
+    local = [x for x in dictionary if checkmaskW(mask,expression,x)]
+    return(local)
+
+def searchW(dictionary,possible=None):
+    if possible==None:
+        possible = dictionary
+    if len(possible) == 1 or len(dictionary) == 1:
+        return([possible[0],0,possible[0] in dictionary])
+    best = None
+    expected = 0
+    length = len(possible)
+    for expression in tqdm(dictionary):
+        localexp = 0
+        allmaskss = allmasksW(expression,possible)
+        for mask in allmaskss:
+            prob = allmaskss[mask]/length
+            localexp += -prob*log2(prob)
+        if localexp > expected or (localexp >= expected and expression in possible):
+            best = expression
+            expected = localexp
+            if len(possible) > 10000:
+                print([best,expected,best in possible])
+    return([best,expected,best in possible])
+
+if __name__ == "__main__":
+    words = []
+    with open("PathToTextFile") as file:
+        for line in file:
+            words.append(line.rstrip())
+    possiblewords = deepcopy(words)
+    print("Number of possible words:",len(possiblewords))
+    #print(searchW(words,possiblewords))
+    best = ['48-32=16']
+    while True:
+        ans1 = input("What you inserted (enter 'b' if it was the best choice): ")
+        ans2 = input("Mask: ")
+        if ans1 == 'b':
+            ans1 = best[0]
+        possiblewords = filtermaskW(ans2,ans1,possiblewords)
+        print("Number of possible words:",len(possiblewords))
+        best = searchW(words,possiblewords)
+        print("Best choice:",best[0])
+        print("Average information:",best[1])
+        if len(possiblewords) < 7:
+            print("Possible words:",possiblewords)
+        print("============")
